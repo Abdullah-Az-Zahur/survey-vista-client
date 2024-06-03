612b716a8453cf8b0f98678ace5993b515e01538
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-06-03 15:53:41 +0600

    creat auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import {
-  RouterProvider,
-} from "react-router-dom";
-import { router } from './routes/Routes';
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Routes";
+import AuthProvider from "./providers/AuthProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <Toaster />
+      </AuthProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
