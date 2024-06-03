7699d72f956379433db29e2b022c7ad1a9a5762f
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-06-03 16:48:09 +0600

    create private routes

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,7 +1,9 @@
+import Banner from "../../components/Banner/Banner";
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <h2>Home asdfsdf</h2>
         </div>
     );
