c452cd959cf39125a97608aca7bd2959880121c0
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-06-03 16:07:23 +0600

    create login

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
     return (
         <div>
-            
+            <Link to='/login'>login</Link>
+            <h2>navbar</h2>
         </div>
     );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,20 +1,23 @@
-import {
-  createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../page/Home/Home";
 import ErrorPage from "../page/ErrorPage/ErrorPage";
+import Login from "../page/Login/Login";
 
-  export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      errorElement:<ErrorPage></ErrorPage>,
-      children:[
-        {
-          path:'/',
-          element:<Home></Home>
-        }
-      ]
-    },
-  ]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login></Login>,
+  },
+]);
