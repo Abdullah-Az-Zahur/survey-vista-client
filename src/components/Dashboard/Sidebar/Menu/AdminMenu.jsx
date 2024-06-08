0869f57593c481a792cdd085b3d4fa3f46eab4ad
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-06-08 13:54:08 +0600

    manage user role

--- src/components/Dashboard/Sidebar/Menu/AdminMenu.jsx
+++ src/components/Dashboard/Sidebar/Menu/AdminMenu.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import { FaUserCog } from 'react-icons/fa';
-import MenuItem from './MenuItem';
+import React from "react";
+import { FaUserCog } from "react-icons/fa";
+import MenuItem from "./MenuItem";
 
 const AdminMenu = () => {
-    return (
-        <div>
-            <MenuItem icon={FaUserCog} label='Manage Users' address='manage-users' />
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <MenuItem
+          icon={FaUserCog}
+          label="Manage Users"
+          address="manage-users"
+        />
+      </div>
+      <div>
+        <MenuItem
+          icon={FaUserCog}
+          label="Surveys Status"
+          address="manage-users"
+        />
+      </div>
+      <div>
+        <MenuItem
+          icon={FaUserCog}
+          label="Payments"
+          address="manage-users"
+        />
+      </div>
+      
+    </div>
+  );
 };
 
 export default AdminMenu;
--- src/page/Dashboard/Admin/ManageUsers.jsx
+++ src/page/Dashboard/Admin/ManageUsers.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "../../../components/Shared/LoadingSpinner";
+import UserDataRow from "../../../components/Dashboard/TableRows/UserDataRow";
 
 const ManageUsers = () => {
+  const axiosSecure = useAxiosSecure();
+  //   Fetch users Data
+  const {
+    data: users = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/users`);
+      return data;
+    },
+  });
+
+  console.log(users);
+  if (isLoading) return <LoadingSpinner />;
+
   return (
     <div>
       <div className="container mx-auto px-4 sm:px-8">
@@ -42,6 +63,15 @@ const ManageUsers = () => {
                   </tr>
                 </thead>
                 <tbody>{/* User data table row */}</tbody>
+                {
+                    users.map(user => (
+                        <UserDataRow
+                        key={user?._id}
+                        user={user}
+                        refetch={refetch}
+                        ></UserDataRow>
+                    ))
+                }
               </table>
             </div>
           </div>
